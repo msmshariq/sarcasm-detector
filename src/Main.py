5895719169e2e23b2034f26658fe2df7fe070930
shariq <msmshariq@gmail.com>
2018-08-02 16:03:21 +0530

    Use GridSearchCV to tune batch_size and epoch

--- src/Main.py
+++ src/Main.py
@@ -19,13 +19,13 @@ import TopicExtractor
 import Utils
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split
-
+from sklearn.model_selection import train_test_split, GridSearchCV
 from keras.models import Model
 from keras.layers import Embedding, Dense, Input, LSTM, Bidirectional, Activation
 from keras.layers.merge import concatenate
 from keras.optimizers import SGD, Adam
 from keras.models import Sequential
+from keras.wrappers.scikit_learn import KerasClassifier
 
 def load_raw_dataset(file_path):
     all_columns = ["label", "comment", "auth", "subreddit", "score", "ups", 
@@ -51,16 +51,17 @@ def get_politics_data():
 #####    
 
 
-def base_model(vocb_size, embedding_matrix, length):
+#def base_model(vocb_size, embedding_matrix, length):
+def base_model():
     model = Sequential()
-    model.add(Embedding(vocab_size, 100, weights=[embedding_matrix], input_length=length, trainable=False))
+    model.add(Embedding(vocab_size, 100, weights=[embedding_matrix], input_length=max_len, trainable=False))
     model.add(Bidirectional(LSTM(20, return_sequences=True)))
     model.add(Dense(20, activation='sigmoid'))
     model.add(Bidirectional(LSTM(20)))
     model.add(Dense(1))
     model.add(Activation('sigmoid'))
     model.compile(loss='binary_crossentropy', optimizer='adam', metrics = ['accuracy'])
-    print(model.summary())
+#    print(model.summary())
     return model 
 
 #input_politics["comment"] = input_politics["comment"].apply(lambda x: Utils.cleanup_str(x))
@@ -80,7 +81,7 @@ vocab_size = 0
 max_len = 0 
 
 def main():
-    global vocab_size, max_len
+    global vocab_size, max_len, tok, embedding_matrix
     input_politics = load_filtered_dataset("/home/shariq/MSc/Research/dataset/train-politics.csv")
     input_politics["all_comments"] = input_politics["parent_comment"].map(str) + " " + input_politics["comment"]
     pick_col = "comment"
@@ -91,12 +92,28 @@ def main():
     print('Max length of sequences : %s' % max_len)
     
     embedding_matrix = Utils.create_embeddings(vocab_size, tok)
-    
-    model = base_model(vocab_size, embedding_matrix, max_len)
     x_train, x_test, y_train, y_test = train_test_split(d, y, test_size=0.2, random_state=0)
-    model.fit(x_train, y_train, epochs=5, batch_size=32)
-    loss, accuracy = model.evaluate(x_test, y_test)
-    print('Accuracy: %f' % (accuracy*100))
+
+    
+#    model = base_model(vocab_size, embedding_matrix, max_len)
+#    model.fit(x_train, y_train, epochs=5, batch_size=32)
+#    loss, accuracy = model.evaluate(x_test, y_test)
+#    print('Accuracy: %f' % (accuracy*100))
+    
+    model = KerasClassifier(build_fn=base_model, verbose=1)
+    # define the grid search parameters
+    batch_size = [32, 64, 128, 256]
+    epochs = [5, 10, 15]
+    param_grid = dict(batch_size=batch_size, epochs=epochs)
+    grid = GridSearchCV(estimator=model, param_grid=param_grid, n_jobs=-1)
+    grid_result = grid.fit(x_train, y_train)
+    # summarize results
+    print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+    means = grid_result.cv_results_['mean_test_score']
+    stds = grid_result.cv_results_['std_test_score']
+    params = grid_result.cv_results_['params']
+    for mean, stdev, param in zip(means, stds, params):
+        print("%f (%f) with: %r" % (mean, stdev, param))
 
 if __name__ == "__main__":
     main()
